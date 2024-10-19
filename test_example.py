c463cd2d40a9d15d96682785df3c9f91046cae46
Tamjid <islamtamjid276@gmail.com>
2024-10-19 19:08:40 +0600

    -

--- test_example.py
+++ test_example.py
@@ -3,11 +3,11 @@
 from example import add, subtract
 
 def test_add():
-    assert add(3, 2) == 4
+    assert add(3, 2) == 5
     assert add(0, 0) == 0
     assert add(-1, 1) == 0
 
 def test_subtract():
-    assert subtract(3, 2) == 3
+    assert subtract(3, 2) == 1
     assert subtract(2, 2) == 0
     assert subtract(0, 1) == -1
